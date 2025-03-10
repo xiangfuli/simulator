7255338caeee31666a70fd3f0203a3af7a3698c9
Xiangfu Li <xiangfuli96@gmail.com>
2025-03-10 17:27:34 +0800

    Specify IP in RPC client

--- airsim/examples/image_request/image_request.cpp
+++ airsim/examples/image_request/image_request.cpp
@@ -6,7 +6,7 @@ int main()
     using namespace msr::airlib;
 
     // for car use CarRpcLibClient
-    MultirotorRpcLibClient client;
+    MultirotorRpcLibClient client("10.42.0.1");
 
     std::vector<uint8_t> png_image = client.simGetImage("0", ImageCaptureBase::ImageType::Scene);
     
